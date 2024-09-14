99c84afd9f40fae9f53423d4456fa0c6ab0df3f6
Anton Dementiev <dementevanton2005@mail.ru>
2024-09-14 17:39:38 +0300

    add ip

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,23 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src import todo_router
 
-app = FastAPI(
-    title="ToDoAppAPI"
-)
+app = FastAPI(title="ToDoAppAPI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173", "http://87.228.19.237/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(
-    todo_router, 
-    prefix="/todo", 
-    tags=["ToDo"]
-)   
+app.include_router(todo_router, prefix="/todo", tags=["ToDo"])
 
 
 if __name__ == "__main__":
